3ea4bed58670b965106ca316f822118d1e6b5316
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-12 13:06:47 +0300

    "put off parcel build for now

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { ApolloProvider } from '@apollo/react-hooks'
-import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { setContext } from 'apollo-link-context'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { Container } from "semantic-ui-react";
 
 const httpLink = createHttpLink({
-  uri: '/graphql'
-})
+  uri: "http://localhost:4000/graphql"
+});
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   return {
     headers: {
       ...headers,
       authorization: token ? `bearer ${token}` : null
     }
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
-})
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -36,5 +36,5 @@ ReactDOM.render(
       </BrowserRouter>
     </Container>
   </ApolloProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
