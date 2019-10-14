82bf66eab04dad45c28b4910af40c4ba69ba5992
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-14 21:30:55 +0300

    moved mongosoe.connection.close()

--- tests/expennses_api.test.js
+++ tests/expennses_api.test.js
@@ -13,16 +13,18 @@ beforeEach(async () => {
   await expense.save()
 })
 
-describe('addition of a new expense', () => {})
-afterAll(async () => {
-  await mongoose.connection.close()
+describe('addition of a new expense', () => {
+  test('all expenses are returned', async () => {
+    const response = await api.get('/api/expenses')
+    expect(response.body.length).toBe(initialExpenses.length)
+  })
+  test('new expense id is formatted correctly', async () => {
+    const response = await api.get('/api/expenses')
+    expect(response.body[0].id).toBeDefined()
+    expect(response.body[0]._id).not.toBeDefined()
+  })
 })
-test('all expenses are returned', async () => {
-  const response = await api.get('/api/expenses')
-  expect(response.body.length).toBe(initialExpenses.length)
-})
-test('new expense id is formatted correctly', async () => {
-  const response = await api.get('/api/expenses')
-  expect(response.body[0].id).toBeDefined()
-  expect(response.body[0]._id).not.toBeDefined()
+
+afterAll(() => {
+  mongoose.connection.close()
 })
