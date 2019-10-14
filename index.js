9fc439fff6d8fba39cbd153fb5adbca62600483c
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-14 13:26:58 +0300

    set up jest, envinroment modes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const config = require('./utils/config')
-
+const axios = require('axios')
 const expenseRouter = require('./controlllers/expense')
 
 const app = express()
@@ -24,15 +24,15 @@ mongoose
     console.log('error connection to MongoDB:', error.message)
   })
 
-// app.get('/api', async (req, res) => {
-//   const user = req.query.user || 'daniel-stafford'
-//   try {
-//     const response = await axios.get(`https://api.github.com/users/${user}`)
-//     res.json({ user: response.data })
-//   } catch (e) {
-//     console.log('error with github get', e.message)
-//   }
-// })
+app.get('/api', async (req, res) => {
+  const user = req.query.user || 'daniel-stafford'
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`)
+    res.json({ user: response.data })
+  } catch (e) {
+    console.log('error with github get', e.message)
+  }
+})
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
