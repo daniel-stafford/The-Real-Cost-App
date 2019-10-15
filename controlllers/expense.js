ff5e4609a91e29efa21486bb179b77806d211a59
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-15 08:06:10 +0300

    expense now has CRUD routes

--- controlllers/expense.js
+++ controlllers/expense.js
@@ -1,6 +1,22 @@
 const expenseRouter = require('express').Router()
 const Expense = require('../models/expense.model')
 
+expenseRouter.post('/', async (request, response, next) => {
+  const { title, price, uses, notes } = request.body
+
+  try {
+    const expense = new Expense({
+      title,
+      price,
+      uses,
+      notes
+    })
+    const savedExpense = await expense.save()
+    response.status(201).json(savedExpense)
+  } catch (error) {
+    console.log(error)
+  }
+})
 expenseRouter.get('/', async (request, response) => {
   try {
     const expenses = await Expense.find({})
@@ -17,21 +33,34 @@ expenseRouter.get('/', async (request, response) => {
   }
 })
 
-expenseRouter.post('/', async (request, response, next) => {
-  const { title, price, uses, notes } = request.body
-
+expenseRouter.put('/:id', async (request, response) => {
+  const expense = {
+    uses: request.body.uses
+  }
+  const updatedExpense = await Expense.findByIdAndUpdate(
+    request.params.id,
+    expense,
+    {
+      new: true
+    }
+  )
   try {
-    const expense = new Expense({
-      title,
-      price,
-      uses,
-      notes
-    })
-    const savedExpense = await expense.save()
-    response.status(201).json(savedExpense)
+    response.json(updatedExpense.toJSON())
   } catch (error) {
     console.log(error)
   }
+})
+
+expenseRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  const expenseToDelete = await Expense.findById(id)
+  try {
+    await Expense.findByIdAndDelete(id)
+    response.json(expenseToDelete.toJSON())
+  } catch (e) {
+    console.log('something went wrong with delete', e.message)
+  }
+  await Expense.findByIdAndDelete(id)
 })
 
 module.exports = expenseRouter
