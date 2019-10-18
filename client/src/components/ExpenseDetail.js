a26fafd8c9a73f00889a3c0420a2e1baef480051
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-18 18:12:38 +0300

    stuck gettign apge to reredner

--- client/src/components/ExpenseDetail.js
+++ client/src/components/ExpenseDetail.js
@@ -7,19 +7,20 @@ import moment from 'moment'
 
 const ExpenseDetail = props => {
   const [expense, setExpense] = useState(null)
-  const [uses, setUses] = useState(props.expense.uses)
+  const [uses, setUses] = useState(null)
   useEffect(() => {
     expenseService.setToken(props.loggedinUser.token)
     expenseService.getAll(props.id).then(response => {
       setExpense(...response.expenses.filter(e => e.id === props.id))
     })
   }, [])
+  setUses(expense.uses)
+
   const handleNewUse = count => {
     console.log('handle new use firing', count)
-    expense.uses = 
   }
 
-  if (!expense) return <Loader active />
+  if (!expense || !uses) return <Loader active />
   return (
     <>
       <ul>
