aa890ba09d614431932b2552e257cf3dfc922e78
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-18 12:46:45 +0300

    slight change to static build function, success!!

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(middleware.requestLogger)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
