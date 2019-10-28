b84bba0c14bc8f3fa807e54371470acf1f8649b5
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-28 17:02:15 +0200

    calendar highlight vanish after refresh

--- client/src/components/ExpenseCalendar.js
+++ client/src/components/ExpenseCalendar.js
@@ -2,25 +2,23 @@ import React from 'react'
 import DayPicker from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
 
-const ExpenseCalendar = props => {
-  console.log('epxense calendar props', props)
-  const birthdayStyle = `.DayPicker-Day--highlighted {
+const ExpenseCalendar = ({ uses }) => {
+  const getModifiers = () => {
+    const modObject = { highlighted: [] }
+    uses.map(date => modObject.highlighted.push(date))
+    console.log('modoject', modObject)
+    return modObject
+  }
+  const highlightStyle = `.DayPicker-Day--highlighted {
   background-color: orange;
   color: white;
 }`
 
-  let myArray = { highlighted: [] }
-  const myModifiers = props.uses.map(date => myArray.highlighted.push(date))
-  console.log('myArray', myArray)
-
-  const modifiers = {
-    highlighted: [new Date(2018, 8, 19), new Date(2018, 8, 20)]
-  }
-  console.log('modifiers', typeof modifiers.highlighted[1])
   return (
     <div>
-      <style>{birthdayStyle}</style>
-      <DayPicker modifiers={myArray} />
+      {console.log('calendar is rendering')}
+      <style>{highlightStyle}</style>
+      <DayPicker modifiers={getModifiers()} />
     </div>
   )
 }
