97cb943c3d187cc6afd0547cb664e3fae26177fb
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-29 14:23:46 +0200

    first styled component rendered, so ugly!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import img from '../images/dollars.jpg'
 
-const Summary = () => {
+const Home = () => {
   const Header = styled.div`
-    background-color: black;
-    color: white
-    min-height: 200px;
-    margin: 30px auto;
-    box-sizing: border-box;
+    height: 95vh;
+    color: white;
+    background-image: linear-gradient(
+        to right bottom,
+        rgba(22, 10, 5, 0.8),
+        rgba(100, 200, 100, 0.8)
+      ),
+      url(${img});
+    background-size: cover;
+    background-position: top;
+    text-align: center;
+    -webkit-clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
+    clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
+    position: relative;
   `
   return (
     <div>
@@ -18,4 +28,4 @@ const Summary = () => {
   )
 }
 
-export default Summary
+export default Home
