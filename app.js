9c9e895d633382e8177829f2d991ad2292e45682
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-30 14:10:46 +0200

    current app on heroku can get data from DB but still fails on refresh

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+
   // app.get('*', (request, response) => {
   //   response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
   // })
