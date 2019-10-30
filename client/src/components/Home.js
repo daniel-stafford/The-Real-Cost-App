42b7ccf70d60f0c437ec6537729b446a33a1bb2f
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-30 08:20:37 +0200

    home page basic adaption from semantic ui example layout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,37 @@
 import React from 'react'
-import styled from 'styled-components'
+import { Container, Header, Button, Icon } from 'semantic-ui-react'
 import img from '../resources/images/dollars.jpg'
+import '../index.css'
 
 const Home = () => {
-  return <h1>The Real Cost App</h1>
+  const HomepageHeading = ({ mobile }) => (
+    <Container text className="header-container">
+      <Header
+        as="h1"
+        content="The Real Cost App"
+        style={{
+          fontSize: mobile ? '2em' : '4em',
+          fontWeight: 'normal',
+          marginBottom: 0,
+          marginTop: mobile ? '1.5em' : '3em'
+        }}
+      />
+      <Header
+        as="h2"
+        content="Track how much your memberships cost per use."
+        style={{
+          fontSize: mobile ? '1.5em' : '1.7em',
+          fontWeight: 'normal',
+          marginTop: mobile ? '0.5em' : '1.5em'
+        }}
+      />
+      <Button primary size="huge">
+        Sign up for Free
+        <Icon name="right arrow" />
+      </Button>
+    </Container>
+  )
+  return <HomepageHeading />
 }
 
 export default Home
