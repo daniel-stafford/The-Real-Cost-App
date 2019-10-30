fb2574d0dac51fef567934faba35cded50d8d76a
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-30 14:04:18 +0200

    db still broken

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ app.use(middleware.requestLogger)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // })
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+  // app.get('*', (request, response) => {
+  //   response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  // })
 }
 
 app.use('/api/expenses', expensesRouter)
