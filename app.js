2f1c1bc2d8dc23b1e96f1e87bfe8d454ccb4a303
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-30 13:53:33 +0200

    production env sendfile tinkering

--- app.js
+++ app.js
@@ -33,8 +33,11 @@ app.use(middleware.requestLogger)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // })
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
 
