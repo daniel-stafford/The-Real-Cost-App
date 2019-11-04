f86c4083cfcc451ab7f11907051dd96ead9b5201
Daniel-Stafford <stafford.daniel@gmail.com>
2019-11-04 20:27:49 +0200

    fiddle with webpack config???

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(middleware.requestLogger)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/../dist/index.html'))
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
   // app.get('*', (request, response) => {
   //   response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
