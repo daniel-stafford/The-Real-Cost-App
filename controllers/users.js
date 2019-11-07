8ef7053be1d707472fa042fc46d7351f6229c131
Daniel-Stafford <stafford.daniel@gmail.com>
2019-11-07 10:51:37 +0200

    still trying to get welcome emiail to work with heroku

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user.model')
 const nodemailer = require('nodemailer')
+const SibApiV3Sdk = require('sib-api-v3-sdk')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({}).populate('blogs', {
@@ -39,6 +40,31 @@ usersRouter.post('/', async (request, response, next) => {
     })
     const savedUser = await user.save()
     response.json(savedUser)
+    //   const defaultClient = SibApiV3Sdk.ApiClient.instance
+    //   const apiKey = defaultClient.authentications['api-key']
+    //   apiKey.apiKey = process.env.SENDINBLUE_API_KEY
+    //   var apiInstance = new SibApiV3Sdk.EmailCampaignsApi()
+    //   var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign()
+
+    //   emailCampaigns.name = 'Campaign sent via the API'
+    //   emailCampaigns.subject = 'My subject'
+    //   emailCampaigns.sender = {
+    //     name: 'From name',
+    //     email: 'stafford.daniel@gmail.com'
+    //   }
+    //   emailCampaigns.type = 'classic'
+    //   emailCampaigns.text = 'blah blah welcome'
+    //   emailCampaigns.
+    // }
+
+    //   apiInstance.createEmailCampaign(emailCampaigns).then(
+    //     function(data) {
+    //       console.log('API called successfully. Returned data: ' + data)
+    //     },
+    //     function(error) {
+    //       console.error(error)
+    //     }
+    //   )
     const transporter = nodemailer.createTransport({
       service: 'gmail', //having isseus with gmail and heroku, try sendinblue....
       auth: {
